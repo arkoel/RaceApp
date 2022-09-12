a66206cd86b48f6545a19741c25324d077d88759
Arkoel <arjenvanwijk03@gmail.com>
2022-09-12 16:53:44 +0200

    hallo

---
+++ Raceapp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Int32 nummer = 255;
+int n = 2;
+System.Boolean ans = true;
+bool a = true;
+System.String s;
+
+Console.Write("Geef een planeet op: ");
+string woord = "Hallo, wereld";
+string? planet = Console.ReadLine();
+Console.WriteLine($"de planeet die jij heb ingetypt is: {planet}!");
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();
